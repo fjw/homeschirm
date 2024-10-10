784009844cd321a0510c8167d388b3e941eac78e
fjw <mail@fworm.dev>
2024-10-10 12:51:40 +0200

    push stuff

--- src/lib/finalize.js
+++ src/lib/finalize.js
@@ -3,9 +3,17 @@ const { cmd } = require('./tools');
 const {pngTmpFile, bmpTmpFile} = require("../config");
 
 exports.convertToBmp = async () => {
-    await cmd("convert", [pngTmpFile, bmpTmpFile]);
+    try {
+        await cmd("convert", [pngTmpFile, bmpTmpFile]);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 exports.pushToDisplay = async () => {
-    await cmd("python3", ["python_src/src/display_bitmap.py"]);
+    try {
+        await cmd("python3", ["python_src/src/display_bitmap.py"]);
+    } catch (e) {
+        console.error(e);
+    }
 }
